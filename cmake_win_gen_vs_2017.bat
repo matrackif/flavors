0398c25cd8ccc73dae65cfc9110c8d40d7f1e4d4
matrackif <filip.matracki@gmail.com>
2019-07-25 16:43:18 +0200

    Slight modifications to cmake files

--- cmake_win_gen_vs_2017.bat
+++ cmake_win_gen_vs_2017.bat
@@ -2,5 +2,5 @@ rem Run this file in a x64 Native Tools Command Prompt for VS on Windows
 set GEN="Visual Studio 15 2017" 
 mkdir build_vs_2017
 cd build_vs_2017
-cmake -D CUDA_INCLUDE_DIRECTORIES="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/include" -D CUDA_LINK_DIRECTORIES="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/lib/x64" -G %GEN% -A x64 ..
+cmake -D CUDA_INCLUDE_DIRS="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/include" -D CUDA_LINK_DIRS="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/lib/x64" -G %GEN% -A x64 ..
 cd ..
